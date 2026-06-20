0dafdc5951c7f204dd96ca1dbba91c30dbb27249
Ryan X. Charles <ryan@ryanxcharles.com>
2026-06-20 05:50:50 -0500

    Bump Shannon to 1.0.0
    Prepare the stable release version and formula.

--- dist/shannon.rb
+++ dist/shannon.rb
@@ -1,8 +1,8 @@
 class Shannon < Formula
   desc "Poly-shell built on nushell with seamless bash compatibility"
   homepage "https://github.com/shannonshell/shannon"
-  url "https://github.com/shannonshell/shannon/releases/download/v0.5.7/shannon-0.5.7.tar.gz"
-  sha256 "9ee34faa76b8a60530f7360d172b1094f02a93e022a7d29decf635d90f9b995c"
+  url "https://github.com/shannonshell/shannon/releases/download/v1.0.0/shannon-1.0.0.tar.gz"
+  sha256 "94fae37468806eb764d9416350e4dbc641598fdaa57e69306084f0b62b1da5c0"
   license "MIT"
 
   depends_on "rust" => :build
@@ -14,7 +14,7 @@ class Shannon < Formula
 
   test do
     version_output = shell_output("#{bin}/shannon --version")
-    assert_match "0.5.7", version_output
+    assert_match "1.0.0", version_output
     assert_match "nushell 0.113.1", version_output
 
     assert_equal "3", shell_output("#{bin}/shannon -c '1 + 2'").strip
